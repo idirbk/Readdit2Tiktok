95eea5f0e35082bccaf2d42272df699a932c19ef
IDRIS ABDELLAH <idrisabdellah@outlook.fr>
2023-03-30 14:19:28 +0200

    editing : init

---
+++ editing/sound.py
@@ -0,0 +1,29 @@
+from moviepy.editor import *
+
+
+def add_sound_to_video(video_path, sound_path, output_path):
+    """
+    This function adds an audio file to a video file and saves the result to a new file.
+
+    Parameters:
+    video_path (str): The path of the video file.
+    sound_path (str): The path of the audio file.
+    output_path (str): The path of the output file.
+
+    Returns:
+    None
+    """
+
+    # Load the video and audio files
+    video_clip = VideoFileClip(video_path)
+    sound_clip = AudioFileClip(sound_path)
+
+    # Add the audio to the video
+    video_clip = video_clip.set_audio(sound_clip)
+
+    # Write the result to a new file
+    video_clip.write_videofile(output_path)
+
+    # Close the clips
+    video_clip.close()
+    sound_clip.close()
